d1a4fdf19af7bfb5abf78f570b28b7e477b0e60e
Charles NDAYISABA <nccharles1@gmail.com>
2020-10-21 15:21:45 +0200

    added UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  goals:string[];
+@Component({ 
+	selector: 'app-root', 
+	templateUrl: './app.component.html', 
+	styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
 
-  constructor(){
-    this.goals = ['Hacking Session', 'Buy New Bundles', 'Test and deploy']
-  } 
-}
+	/* An empty array that is responsible 
+	to add a division */
+	public items = []; 
+
+	/* A two-way binding performed which 
+	pushes text on division */
+	public newGoal; 
+
+
+	/* When input is empty, it will 
+	not create a new division */
+	public addToList() { 
+		if (this.newGoal == '') { 
+		} 
+		else { 
+			this.items.push(this.newGoal); 
+			this.newGoal = ''; 
+		} 
+	} 
+
+	/* This function takes to input the 
+	task, that has to be deleted*/
+	public deleteGoal(index) { 
+    let toDelete = confirm(`Are you sure you want to delete ${this.items[index]}?`)
+
+      if (toDelete){
+        this.items.splice(index, 1); 
+      }
+		
+	} 
+} 
