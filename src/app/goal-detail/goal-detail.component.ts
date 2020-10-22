72940b3d3b5fb7b317132b3f4ed45fd2186fa6dd
Charles NDAYISABA <nccharles1@gmail.com>
2020-10-22 13:05:06 +0200

    user can be able to delete Goal

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -14,6 +14,9 @@ export class GoalDetailComponent implements OnInit {
   goalComplete(complete:boolean){
     this.isComplete.emit(complete);
   }
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
